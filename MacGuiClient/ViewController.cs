b795f92697f5e15747557fa162386ab2f136f0ae
Thomas Peterson <thomas@Thomass-MacBook-Pro.fritz.box>
2017-03-25 21:03:00 +0100

    Backup

---
+++ MacGuiClient/ViewController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using AppKit;
+using Common;
+using Foundation;
+
+namespace MacGuiClient
+{
+	public partial class ViewController : NSViewController
+	{
+		public ClientThread Manager { get { return Common.Instance.Client.Instance.Thread; } }
+
+		protected Common.Config.Manager ConfigManager;
+
+		public ViewController(IntPtr handle) : base(handle)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+
+		}
+
+		public override NSObject RepresentedObject
+		{
+			get
+			{
+				return base.RepresentedObject;
+			}
+			set
+			{
+				base.RepresentedObject = value;
+				// Update the view, if already loaded.
+			}
+		}
+	}
+}
