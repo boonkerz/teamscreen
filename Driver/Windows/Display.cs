f98ac366d040e20cbb578841df54981610eac20d
Thomas Peterson <info@thomas-peterson.de>
2017-03-25 21:51:49 +0100

    Backup

--- Driver/Windows/Display.cs
+++ Driver/Windows/Display.cs
@@ -8,31 +8,82 @@ namespace Driver.Windows
 {
 	public class Display : Driver.Interfaces.Display
 	{
-
-        System.Drawing.Rectangle bounds;
-        DesktopMirror dfmirage;
+        DesktopMirror MirrorDriver;
 
 
         public Display()
 		{
-			bounds = Screen.GetBounds(System.Drawing.Point.Empty);
-            dfmirage = new DesktopMirror();
-            Console.WriteLine(dfmirage.DriverExists());
+			MirrorDriver = new DesktopMirror();
 		}
 
-		public int getScreenHeight()
-		{
-			return bounds.Height;
-		}
+        public bool DoesMirrorDriverExist()
+        {
+            return MirrorDriver.DriverExists();
+        }
 
-		public int getScreenWidth()
+        public makeScreenshot()
 		{
-			return bounds.Width;
-		}
 
-		public byte[] makeScreenshot()
-		{
-			byte[] image = new byte[] { };
+            if(DoesMirrorDriverExist())
+            {
+
+
+
+            }
+
+            var stream = new System.IO.MemoryStream();
+
+            if (MirrorDriver.State != DesktopMirror.MirrorState.Running)
+            {
+                // Most likely first time
+                // Start the mirror driver
+                MirrorDriver.Load();
+
+                MirrorDriver.Connect();
+                MirrorDriver.Start();
+
+                Bitmap screenshot = MirrorDriver.GetScreen();
+                screenshot.Save(stream, ImageFormat.Png);
+
+                return stream.ToArray();
+            }
+            else if (MirrorDriver.State == DesktopMirror.MirrorState.Running)
+            {
+                // Send them the list of queued up changes
+                var regions = (List<Rectangle>)GetOptimizedRectangleRegions();
+
+                Bitmap screenshot = MirrorDriver.GetScreen();
+
+                if (regions.Count == 0)
+                {
+                    Network.SendMessage(new ResponseEmptyScreenshotMessage(), NetDeliveryMethod.ReliableOrdered, 0);
+                    return;
+                }
+
+                for (int i = 0; i < regions.Count; i++)
+                {
+                    if (regions[i].IsEmpty) continue;
+
+                    Bitmap regionShot = null;
+
+                    try
+                    {
+                        regionShot = screenshot.Clone(regions[i], PixelFormat.Format16bppRgb565);
+                    }
+                    catch (OutOfMemoryException ex)
+                    {
+                        
+                    }
+                    
+                    regionShot.Save(stream, ImageFormat.Png);
+
+                    
+                }
+            }
+
+            return stream.ToArray();
+            /*
+            byte[] image = new byte[] { };
 
 			var bmpScreenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
 							   Screen.PrimaryScreen.Bounds.Height,
@@ -49,7 +100,6 @@ namespace Driver.Windows
 										Screen.PrimaryScreen.Bounds.Size,
 										CopyPixelOperation.SourceCopy);
 
-			var stream = new System.IO.MemoryStream();
 			ImageCodecInfo jpgEncoder = GetEncoder(ImageFormat.Jpeg);
 
 			// Create an Encoder object based on the GUID
@@ -70,7 +120,8 @@ namespace Driver.Windows
 
 			image = stream.ToArray();
 			return image;
-		}
+            */
+        }
 
 		private ImageCodecInfo GetEncoder(ImageFormat format)
 		{
